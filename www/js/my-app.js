14e02c45f6e8dc6b89a964fbec89801888aa5a41
AJ Pasigado <ajpasigado@outlook.com>
2019-02-05 23:43:26 +0800

    UI Changes

---
+++ www/js/my-app.js
@@ -0,0 +1,51 @@
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My Wedding',
+    // App id
+    id: 'com.ajpasigado.mywedding',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/home/',
+        url: 'home.html',
+      },
+      {
+        path: '/event-details/',
+        url: 'event-details.html',
+      },
+      {
+        path: '/our-story/',
+        url: 'our-story.html',
+      },
+      {
+        path: '/guests/',
+        url: 'guests.html',
+      },
+      {
+        path: '/faq/',
+        url: 'faq.html',
+      },
+      {
+        path: '/menu/',
+        url: 'menu.html',
+      },
+      {
+        path: '/venue/',
+        url: 'venue.html',
+      },
+      
+      {
+        path: '/test/',
+        url: 'test.html',
+      },
+    ],
+    // ... other parameters
+  });
+  
+  var mainView = app.views.create('.view-main');
