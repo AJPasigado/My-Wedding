fe5c4f0933e2075a96f7c624daa85cc0fb9373e1
AJ Pasigado <ajpasigado@outlook.com>
2019-02-09 17:17:59 +0800

    Finished all UI Elements; Replaced stock images; Semi-Final build

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -49,3 +49,81 @@ var app = new Framework7({
   });
   
   var mainView = app.views.create('.view-main');
+
+  var swiper = app.swiper.get('.swiper-container');
+
+  function clickSlide(a){
+    for (i = 0; i < a; i++) {
+      swiper.slideNext();
+    }
+    
+  }
+
+  function prevSlide(a){
+    for (i = 0; i < a; i++) {
+      swiper.slidePrev();
+    }
+  }
+
+$(function() {
+  $("#guests").on("scroll", function() {
+    if($("#guests").scrollTop() > 1) {
+        $("#guests-bar").addClass("guest-active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $("#guests-bar").removeClass("guest-active");
+    }
+});
+
+  $("#venue").on("scroll", function() {
+      if ($("#venue").scrollTop() > 150) {
+          $("#venue-church").addClass("title-active");
+      } else {
+        $("#venue-church").removeClass("title-active");
+     }
+      
+      if($("#venue").scrollTop() > 900) {
+          $("#venue-reception").addClass("title-active");
+      } else {
+         $("#venue-reception").removeClass("title-active");
+      }
+  });
+
+  $("#menu").on("scroll", function() {
+    if ($("#menu").scrollTop() > 10) {
+        $("#menu-appetizer").addClass("title-active");
+    } else {
+      $("#menu-appetizer").removeClass("title-active");
+   }
+    
+    if($("#menu").scrollTop() > 300) {
+        $("#menu-main").addClass("title-active");
+    } else {
+       $("#menu-main").removeClass("title-active");
+    }
+  });
+
+  $("#story").on("scroll", function() {
+    if ($("#story").scrollTop() > 1) {
+        $("#story-div").addClass("gradient-bg");
+    } else {
+      $("#story-div").removeClass("gradient-bg");
+   }
+  });
+
+  $("#home").on("scroll", function() {
+    if ($("#home").scrollTop() > 1) {
+        $("#home-div").addClass("gradient-bg");
+    } else {
+      $("#home-div").removeClass("gradient-bg");
+   }
+  });
+
+  $("#event").on("scroll", function() {
+    if ($("#event").scrollTop() > 1) {
+        $("#event-div").addClass("gradient-bg");
+    } else {
+      $("#event-div").removeClass("gradient-bg");
+   }
+  });
+});
